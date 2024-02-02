2ef5e75979f2101df94756402d140fa7aaec7d6f
SunChenYang <171928131@qq.com>
2024-02-02 13:48:47 +0800

    ✨ feat: 新春主题

--- userscript/src/userscript-header.js
+++ userscript/src/userscript-header.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         Copy Zentao BUG
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.4
 // @description  禅道每日bug一键复制
 // @author       Sun chen yang
 // @match        http://dev.veilytech.com/zentao/*
 // @match        http://114.215.30.227:8081/zentao/*
+// @match        http://47.96.22.123/zentao/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=veilytech.com
 // @grant        none
 // @license MIT
